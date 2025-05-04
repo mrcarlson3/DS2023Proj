7ed446b543113ac02c8d497e8490172132862be4
Michael Carlson <mcarlsonr5@gmail.com>
2025-05-04 19:22:24 -0400

    setting up final report

--- datalearning.py
+++ datalearning.py
@@ -1,20 +1,18 @@
 ## Cleaning the data
-```{python}
+
 # import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import scipy as sp
-```
-```{python}
+
 # load data
 c1 = pd.read_csv('catapult season 1.csv')
 c2 = pd.read_csv('catapult season 2.csv')
 # merge the two seasons using concat ignoring their individual indexes
 df = pd.concat([c1, c2], ignore_index=True)
-```
-```{python}
+
 #check for duplicates
 duplicates = df.duplicated()
 print(f"Number of duplicates: {duplicates.sum()}")
@@ -30,4 +28,4 @@ key_vars = [
     'Total Player Load', 'Player Load Per Minute', 'Explosive Efforts', 'Session Total Jump', 'Session Jumps Per Minute', 'Total IMA', 'IMA/Min', 'Position'
 ]
 df = df[key_vars]
-```
+
